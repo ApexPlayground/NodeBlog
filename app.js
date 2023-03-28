e591d2662f5d0e06bc5949421fd2d0decd4b2f4d
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-28 18:46:12 +0100

    made edge work

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 const express = require('express');
 const app = express();
 
-// Import the express-edge module and configure it as the view engine
-const {config, engine} = require('express-edge');
-app.use(engine);
+// Import the edge module and configure it as the view engine
+const { config, engine } = require('express-edge');
 
-const {showHomePage} = require('./controllers/PostController');
+const {showHomePage, createPost} = require('./controllers/PostController');
 
 const db = require('./db');
 
 // Serve static files located in the "public" folder
 app.use(express.static('public'));
+
+app.use(engine);
 
 // Set the directory where the application's views are located
 app.set('views', `${__dirname}/views`);
@@ -19,7 +20,9 @@ app.set('views', `${__dirname}/views`);
 // Set up a route for the root URL that renders the "index" view
 app.get('/', showHomePage);
 
+app.get('/posts/new', createPost);
+
 // Start the web server on port 3000 and log a message to the console
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log('Server started on port 3000');
 });
