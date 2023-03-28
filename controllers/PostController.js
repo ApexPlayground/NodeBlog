ab0c12229b7736f5b94cd3f43f95b75fd3f1ec94
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-28 18:46:39 +0100

    added create post page to app

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,8 +1,11 @@
 const showHomePage = (req, res) => {
     res.render('index');
 }
+const createPost = (req, res) => {
+    res.render('add_post');
+}
 
 module.exports = {
-    showHomePage
+    showHomePage,createPost
 }
 
