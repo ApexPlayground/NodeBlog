06c163386c2fb1fc28bb8dbadeb2030815bf7c4d
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-28 19:16:07 +0100

    storing data to database

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,11 +1,21 @@
+const Post = require('../models/Post');
 const showHomePage = (req, res) => {
     res.render('index');
 }
 const createPost = (req, res) => {
     res.render('add_post');
 }
+const storePost = async (req,res) => {
+    try{
+        console.log(req.body);
+        res.redirect('/');
+        //post.create(req.body);
+    } catch (error){
+        console.log(error);
+    }
+}
 
 module.exports = {
-    showHomePage,createPost
+    showHomePage,createPost,storePost
 }
 
