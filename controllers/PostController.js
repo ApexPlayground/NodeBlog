1cb6541759c0305a00d2dac93a7d6096a4fbfaac
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-29 16:03:54 +0100

    displayed data from database to app

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,27 +1,31 @@
 const path = require('path');
 const Post = require('../models/Post');
-const showHomePage = (req, res) => {
-    res.render('index');
+const showHomePage = async (req, res) => {
+    const posts = await Post.find({});
+    res.render('index', {posts});
 }
 const createPost = (req, res) => {
     res.render('add_post');
 }
 const storePost = async (req, res) => {
     try {
-        const { image } = req.files;
+        const {image} = req.files;
         await image.mv(path.resolve(__dirname, '..', 'public/posts', image.name));
         await Post.create({
             ...req.body,
             image: `/posts/${image.name}`,
         });
         res.redirect('/');
-        //post.create(req.body);
     } catch (error) {
         console.log(error);
     }
 }
+const showPost = async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('post', {post});
+}
 
 module.exports = {
-    showHomePage, createPost, storePost
+    showHomePage, createPost, storePost, showPost
 }
 
