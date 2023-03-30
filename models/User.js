f6e664be2404fcd5f7287d671d1e33650246e6cc
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-30 18:15:25 +0100

    added email validator and coments

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+// Require the necessary modules
+const mongoose = require("mongoose"); // Mongoose for object modeling with MongoDB
+const bcrypt = require("bcrypt"); // Bcrypt for password hashing
 const Schema = mongoose.Schema;
 
+// Define the user schema
 const UserSchema = new Schema({
   Username: {
     type: String,
@@ -12,6 +14,13 @@ const UserSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    // Add an email validator using the Mongoose built-in validator with the 'match' option
+    validate: {
+      validator: function(v) {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: "Please enter a valid email address"
+    }
   },
   password: {
     type: String,
@@ -23,17 +32,25 @@ const UserSchema = new Schema({
   },
 });
 
+// Add a pre-save hook to hash the user's password before saving it to the database
 UserSchema.pre("save", function(next){
+    // Check if the password has been modified before hashing it
     if(!this.isModified("password")){
         return next();
     }
+    // Hash the password using bcrypt with a salt of 10
     bcrypt.hash(this.password, 10, (err, passwordHash) => {
         if(err){
             return next(err);
         }
+        // Set the user's password to the hashed password
         this.password = passwordHash;
         next();
     });
 });
+
+// Create a Mongoose model for the user schema
 const User = mongoose.model("User", UserSchema);
+
+// Export the User model to be used in other parts of the application
 module.exports = User;
