6528febe07d96505e195c131b59c833948ef6d96
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-03-31 10:12:58 +0100

    added error handling and linked login form submission

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Import controller modules
 const { showHomePage, createPost, storePost, showPost } = require('./controllers/PostController');
-const { createUser, storeUser, showLogin } = require('./controllers/UserController');
+const { createUser, storeUser, showLogin, loginUser } = require('./controllers/UserController');
 
 // Set up routes
 app.get('/', showHomePage); // Route for the root URL that renders the "index" view
@@ -32,9 +32,16 @@ app.get('/posts/:id', showPost); // Route for showing a specific post
 app.get('/auth/register', createUser); // Route for the "register" page
 app.post('/auth/register', storeUser); // Route for storing a new user
 app.get('/auth/login', showLogin); // Route for the "login" page
+app.post('/auth/login', loginUser);// Route to handle login form submission 
 
 // Set up middleware for handling file uploads
 app.use(expressFileUpload());
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 // Start the web server on port 3000 and log a message to the console
 app.listen(3000, () => {
