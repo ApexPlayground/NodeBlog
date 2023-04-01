3b0eefab9bb46e5a959d7419f7bc1cc8acd851bc
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-01 11:01:27 +0100

    little updates

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,27 @@
+// Require the User model
 const User = require("../models/User")
+
+// Define an async middleware function to authenticate the user
 const authenticateUser = async (req, res, next) => {
+
+    // Check if the user ID is stored in the session
     if (req.session.userId) {
+
+        // If the user ID is found, try to find the user in the database
         const user = await User.findById(req.session.userId);
+
+        // If a user is found, redirect to the home page
         if (user) {
             res.redirect("/")
         } else {
+            // If the user is not found, continue to the next middleware
             next();
         }
     } else {
+       // If there is no user ID in the session, continue to the next middleware
        next();
     }
 };
 
+// Export the authenticateUser middleware function
 module.exports = { authenticateUser };
--- middlewares/redirect.js
+++ middlewares/redirect.js
@@ -1,7 +1,13 @@
+// Export a middleware function that checks if the user is logged in
 module.exports = (req, res, next) => {
+
+    // Check if the user ID is stored in the session
     if (req.session.userId){
+
+        // If the user ID is found in the session, continue to the next middleware
         next();
     }else{
+        // If there is no user ID in the session, redirect to the login page
         res.redirect("/auth/login");
     }
 };
