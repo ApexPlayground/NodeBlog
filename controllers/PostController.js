931ef49c7e138b2fd0b3ef91d514fc2446a49043
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-04-01 15:59:57 +0100

    edit post logic implemented

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -10,7 +10,7 @@ const showHomePage = async (req, res) => {
     const posts = await Post.find({});
 
     // Render the home page template and pass in the posts
-    res.render('index', {posts});
+    res.render('index', { posts });
 }
 
 // Define a function to render the form for creating a new post
@@ -23,7 +23,7 @@ const createPost = (req, res) => {
 const storePost = async (req, res) => {
     try {
         // Get the image file from the request
-        const {image} = req.files;
+        const { image } = req.files;
 
         // Move the image file to the public/posts folder
         await image.mv(path.resolve(__dirname, '..', 'public/posts', image.name));
@@ -48,37 +48,61 @@ const showPost = async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     // Render the post template and pass in the post data
-    res.render('post', {post});
+    res.render('post', { post });
 }
 
 // Define an async function to delete a post from the database
 const deletePost = async (req, res) => {
     try {
-    const post = await Post.findByIdAndDelete(req.params.id);
-    if (!post) {
-      return res.status(404).send("Post not found");
+        const post = await Post.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send("Post not found");
+        }
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error deleting post");
     }
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("Error deleting post");
-  }
 };
 
 // Define an async function to edit a post from the database
 const editPost = async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        res.render('edit_post', {post: post});
+        res.render('edit_post', { post: post });
     } catch (err) {
         console.log(err);
     }
 };
 
+const updatePost = async (req, res) => {
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+          title: req.body.title,
+          description: req.body.description,
+          content: req.body.content,
+          image: req.body.image
+        },
+        { new: true } // to return the updated document
+      );
   
+      res.redirect('/posts/' + updatedPost._id);
+    } catch (err) {
+      console.log(err);
+      res.redirect('/posts/' + req.params.id + '/edit');
+    }
+  };
+
+// Require the express-fileupload middleware to handle file uploads
+const fileUpload = require('express-fileupload');
   
+
+
+
 
 // Export the showHomePage, createPost, storePost, and showPost functions
 module.exports = {
-    showHomePage, createPost, storePost, showPost, deletePost, editPost
+    showHomePage, createPost, storePost, showPost, deletePost, editPost, updatePost
 }
